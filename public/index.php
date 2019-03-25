5dbafc64003b492902e93e4f537f73436d6e8d68
Audrey <audrey@mail.com>
2019-03-25 16:39:21 +0100

    version creation de routes

--- public/index.php
+++ public/index.php
@@ -4,15 +4,34 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Slim\App;
 
-//Autoload
-require_once dirname(__DIR__) . "/vendor/autoload.php";
+require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 //création de l'application
-$app = new App();
-//alt enter pour rajouter automatiquement les classes
 
-$app->get('/hello', function (ServerRequestInterface $request, ResponseInterface $response, ?array $args) {
-    return $response->getBody()->write('<h1>Bonjour</h1>');
+$app = new App();
+
+$route = $app->get('/', function ( ServerRequestInterface $request, ResponseInterface $response, ?array $args
+) {
+    return $response->getBody()->write('<h1>Hello</h1>');
 });
-// lancement de l'application. Création et renvoie de la réponse au navigateur
+$route->setName('homepage');
+
+
+$app->group('/projet', function () {
+    $this->get("/{id:\d+}", function ( ServerRequestInterface $request, ResponseInterface $response, ?array $args
+    ) {
+        return $response->getBody()->write('<h1>Détail du projet</h1>');
+    }) ->setName('app_project_show');
+
+    $this->get("/creation", function ( ServerRequestInterface $request, ResponseInterface $response, ?array $args
+    ) {
+        return $response->getBody()->write('<h1>Création d\'un projet</h1>');
+    }) ->setName('app_project_create');
+});
+
+
+
+
+
+//création et renvoi de la réponse au navigateur
 $app->run();
