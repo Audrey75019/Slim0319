4d6d938e2929642925165523a619cfd5238b2d37
Audrey <audrey@mail.com>
2019-03-25 14:46:46 +0100

    version sniffer

--- public/index.php
+++ public/index.php
@@ -1,5 +1,18 @@
 <?php
 
-echo "bonjour";
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\App;
 
-?>
+//Autoload
+require_once dirname(__DIR__) . "/vendor/autoload.php";
+
+//création de l'application
+$app = new App();
+//alt enter pour rajouter automatiquement les classes
+
+$app->get('/hello', function (ServerRequestInterface $request, ResponseInterface $response, ?array $args) {
+    return $response->getBody()->write('<h1>Bonjour</h1>');
+});
+// lancement de l'application. Création et renvoie de la réponse au navigateur
+$app->run();
